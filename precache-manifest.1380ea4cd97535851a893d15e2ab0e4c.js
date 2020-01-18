eb3c92d5374ea556d6350e3834f47e471aa9b01c
TatjanaLukic <id4tanja@gmail.com>
2020-01-18 15:09:50 +0100

    Updates

---
+++ precache-manifest.1380ea4cd97535851a893d15e2ab0e4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0ca03de3801dcc636025fd28a15bb9",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "0bca86e65d30a1aecea1",
+    "url": "/quiz/static/js/2.688641a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz/static/js/2.688641a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f292a3d7885c4b58fca9",
+    "url": "/quiz/static/js/main.9e251c27.chunk.js"
+  },
+  {
+    "revision": "e760403150b6dd923f80",
+    "url": "/quiz/static/js/runtime-main.c3272f72.js"
+  }
+]);
