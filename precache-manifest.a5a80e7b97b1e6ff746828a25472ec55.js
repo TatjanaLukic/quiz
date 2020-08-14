67c0e0a809b4083f1e2ba70eb2c121b78835019e
TatjanaLukic <id4tanja@gmail.com>
2020-08-14 11:30:55 +0200

    Updates

---
+++ precache-manifest.a5a80e7b97b1e6ff746828a25472ec55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb7686fccc03b8af23d3",
+    "url": "/quiz/static/css/main.7d3d24f1.chunk.css"
+  },
+  {
+    "revision": "bb7686fccc03b8af23d3",
+    "url": "/quiz/static/js/main.56e76957.chunk.js"
+  },
+  {
+    "revision": "5898efb7f0a1a3dff560",
+    "url": "/quiz/static/js/runtime~main.e6ae5a1e.js"
+  },
+  {
+    "revision": "c8599c1dc0f41a8c48b7",
+    "url": "/quiz/static/js/2.905d1f3e.chunk.js"
+  },
+  {
+    "revision": "699c151c94c2eee8616dec769067da21",
+    "url": "/quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.a816b082bdb327e0af86428210ea7867.js"
+  "/quiz/precache-manifest.a5a80e7b97b1e6ff746828a25472ec55.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz/index.html"), {
+workbox.routing.registerNavigationRoute("/quiz/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
