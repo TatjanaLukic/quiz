f7a94e450058125183e6a88c986d7a5ff14499fb
TatjanaLukic <id4tanja@gmail.com>
2020-09-02 21:07:22 +0200

    test existance of each link in Navabr

--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -9,10 +9,31 @@ import {Link} from 'react-router-dom';
 configure({adapter: new Adapter()});
 
 describe('<Navbar/>',() => {
+    let wrapper ;
+
+    beforeEach( () => {
+        wrapper = shallow(<Navbar/>);
+    });
+
     it('should render four <Link/> elements', ()=>{
-        const wrapper = shallow(<Navbar/>);
         expect(wrapper.find(Link)).toHaveLength(4); 
-    }
-    )
+    });
+
+    it('should contain <Link to ="/">Home</Link>', ()=>{     
+        expect(wrapper.contains(<Link to ="/">Home</Link>)).toEqual(true);
+    });
+
+    it('should contain <Link to ="/JavaScriptQuiz">JavaScript</Link>', ()=>{     
+        expect(wrapper.contains(<Link to ="/JavaScriptQuiz">JavaScript</Link>)).toEqual(true); 
+    });
+
+    it('should contain <Link to ="/ReactQuiz">React</Link>', ()=>{    
+        expect(wrapper.contains(<Link to ="/ReactQuiz">React</Link>)).toEqual(true); 
+    });
+
+    it('should contain <Link to ="/CSSQuiz">CSS</Link>', ()=>{     
+        expect(wrapper.contains(<Link to ="/CSSQuiz">CSS</Link>)).toEqual(true); 
+    });
+
 
 })
