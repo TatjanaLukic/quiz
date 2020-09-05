c80b46744e308d95eff56966aa24dc31e3cfb4dd
TatjanaLukic <id4tanja@gmail.com>
2020-09-05 20:54:21 +0200

    added testing of Result component in CSSQuiz

--- src/__tests__/CSSQuiz.test.js
+++ src/__tests__/CSSQuiz.test.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { configure, shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount } from "enzyme";
+import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
 
-import CSSQuiz from '../components/quizes/CSSQuiz';
+import CSSQuiz from "../components/quizes/CSSQuiz";
+import Result from "../components/Result";
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
-describe('<CSSQuiz/>',() => {
-
-it("spanshot matches", () => {
+describe("<CSSQuiz/>", () => {
+  it("spanshot matches", () => {
     const tree = shallow(<CSSQuiz />);
     expect(toJson(tree)).toMatchSnapshot();
+  });
+
+  it("no Result component on first load", () => {
+    const tree = shallow(<CSSQuiz />);
+    expect(tree.find(Result)).toHaveLength(0);
+  });
+
+  it("Result exists when clicked submit button", () => {
+    const tree = mount(<CSSQuiz />);
+    tree.find('button').simulate('click');
+    expect(tree.find(Result)).toHaveLength(1);
   });
 
 });
